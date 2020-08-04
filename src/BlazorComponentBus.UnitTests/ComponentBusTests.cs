a2f6510eb16597e52cef985266088de1c9a06613
Justin Basinger <justin.m.basinger@gmail.com>
2020-08-03 20:27:47 -0400

    Adding a generic subscribe method to match the generic publish. Adding a general visual studio .gitignore file.

--- src/BlazorComponentBus.UnitTests/ComponentBusTests.cs
+++ src/BlazorComponentBus.UnitTests/ComponentBusTests.cs
@@ -16,6 +16,18 @@ namespace BlazorComponentBus.UnitTests
 
             Assert.Equal(1, subscriber.Count);
         }
+
+        [Fact]
+        public async Task ShouldAllowGenericSubscribeToAndPublishAnEvent()
+        {
+          var bus = new ComponentBus();
+          var publisher = new PublishingComponent(bus);
+          var subscriber = new GenericSubscribingComponent(bus);
+
+          await publisher.PublishEvent();
+
+          Assert.Equal(1, subscriber.Count);
+        }
     }
 
     public class SubscribingComponent
@@ -45,6 +57,21 @@ namespace BlazorComponentBus.UnitTests
         public async Task PublishEvent()
         {
            await _bus.Publish(new TestEventMessage());
+        }
+    }
+
+    public class GenericSubscribingComponent
+	  {
+        public int Count { get; set; }
+
+        public GenericSubscribingComponent(ComponentBus bus)
+        {
+            bus.Subscribe<TestEventMessage>((sender, e) => ReceiveMessage(e));
+        }
+
+        public void ReceiveMessage(MessageArgs args)
+        {
+            Count++;
         }
     }
 
