6ad9271a38db51c3790a76e6a23156672175290b
Chris Bush <cbush@aiconline.com>
2022-09-28 09:41:12 -0500

    #7 Added SubscribeTo and UnsubscribeFrom methods that eliminate the need for marshalling MessageArgs around

--- src/BlazorComponentBus/IComponentBus.cs
+++ src/BlazorComponentBus/IComponentBus.cs
@@ -4,9 +4,14 @@ namespace BlazorComponentBus;
 
 public interface IComponentBus
 {
-    Task Publish<T>(T message, CancellationToken ct = default);
+    Task Publish<T>(T message);
+    Task Publish<T>(T message, CancellationToken ct);
     void Subscribe<T>(AsyncComponentCallBack<MessageArgs> componentCallback);
     void Subscribe<T>(ComponentCallBack<MessageArgs> componentCallback);
+    void SubscribeTo<T>(AsyncComponentCallBack<T> componentCallback);
+    void SubscribeTo<T>(ComponentCallBack<T> componentCallback);
     void UnSubscribe<T>(AsyncComponentCallBack<MessageArgs> componentCallBack);
     void UnSubscribe<T>(ComponentCallBack<MessageArgs> componentCallBack);
+    void UnSubscribeFrom<T>(AsyncComponentCallBack<T> componentCallback);
+    void UnSubscribeFrom<T>(ComponentCallBack<T> componentCallback);
 }
